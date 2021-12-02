35992d37aa5cb0c40e916f756582c9b03838a8df
sanwaldev <sanwalbcs@gmail.com>
2021-12-02 12:21:12 +0500

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -36,8 +36,6 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => {
-            console.log(this.state);
           });
         });
       }else{
