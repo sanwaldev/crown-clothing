d82f7c13c494cca68d9102dccbc8d1c664b91373
sanwaldev <sanwalbcs@gmail.com>
2021-12-02 15:52:49 +0500

    updated app to redirect to if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -49,7 +49,11 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} ></Route>
           <Route path="/shop" component={ShopPage} ></Route>
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route exact path="/signin" 
+          render={
+            () => this.props.currentUser ? 
+            (<Redirect to='/' />) : 
+            (<SignInAndSignUpPage />)} />
 
         </Switch>
       </div>
@@ -57,8 +61,15 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatcToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatcToProps)(App);
+export default connect(
+  mapStateToProps, 
+  mapDispatcToProps)
+  (App);
