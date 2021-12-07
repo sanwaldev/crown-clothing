d404edc3037392697f99ae5a219458eeea6290bb
sanwaldev <sanwalbcs@gmail.com>
2021-12-07 16:29:33 +0500

    created collections overview component initiated map in it by moving map function form shop component to collection overview component

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-
-import '../../components/collection-preview/collection-preview.component.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.js';
-
-import { selectCollections } from '../../redux/shop/shop.selector.js';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
  
 const ShopPage = ({ collections }) => (
     <div className="shop-page">
-        {collections.map(({id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-        }
+        <CollectionOverview />
     </div>
 );
 
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-})
-export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
